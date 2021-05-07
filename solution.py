2e90c15ab1cac7f9e83a11eac72bef6282156e90
onlinejudge95 <onlinejudge95@gmail.com>
2021-05-07 23:43:27 +0530

    Adds function docstring

--- solution.py
+++ solution.py
@@ -1,10 +1,17 @@
 from itertools import combinations
 
 
+# Global variable to store the count
+# It's not a good idea I understand.
 COUNT = 0
 
 
 def _get_subsets(array: list, data: list, start: int, end: int, index: int, size: int):
+    """
+    Recursive utiltiy method to find the subsets with a given length.
+    Once the subset matches check if the sum is less than or equal to 1000
+    If yes bump the count by 1
+    """
     if index == size:
         if sum(data[:size]) <= 1000:
             global COUNT
@@ -19,6 +26,9 @@ def _get_subsets(array: list, data: list, start: int, end: int, index: int, size
 
 
 def solve(array: list, size: int) -> None:
+    """
+    Driver function to call the recursive function and print the count for the same.
+    """
     data = [0] * size
 
     _get_subsets(array, data, 0, len(array) - 1, 0, size)
