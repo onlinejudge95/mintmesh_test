25b329005f556fa811303b423469c27a7df0a58b
onlinejudge95 <onlinejudge95@gmail.com>
2021-05-08 17:41:55 +0530

    scraping implementation complete

--- src/blueprints/api/lib.py
+++ src/blueprints/api/lib.py
@@ -2,22 +2,24 @@ def get_all_nodes(nodes):
     result = []
 
     for node in nodes:
+        data_nodes = node.find_all("td")
         try:
-            data_nodes = node.find_all("td")
-            total_cases = int(data_nodes[2].contents[0].replace(",", ""))
-            total_recovered = int(data_nodes[6].contents[0].replace(",", ""))
-            population = int(data_nodes[14].contents[0].replace(",", ""))
+            country_name = data_nodes[1].find("a").contents[0]
+            total_cases = int(data_nodes[2].contents[0].replace(",", "").strip())
+            active_cases = int(data_nodes[8].contents[0].replace(",", "").strip())
+            total_deaths = int(data_nodes[4].contents[0].replace(",", "").strip())
+            total_recovered = int(data_nodes[6].contents[0].replace(",", "").strip())
+            population = int(data_nodes[14].find("a").contents[0].replace(",", "").strip())
             temp = {
-                "country_name": data_nodes[1].find("a").contents[0],
+                "country_name": country_name,
                 "total_cases": total_cases,
-                "active_cases": int(data_nodes[8].contents[0].replace(",", "")),
-                "total_deaths": int(data_nodes[4].contents[0].replace(",", "")),
+                "active_cases": active_cases,
+                "total_deaths": total_deaths,
                 "recovery_rate": round((total_recovered * 100.0) / total_cases, 2),
                 "infected_population_percentage": round((total_cases * 100.0) / population, 2)
             }
             result.append(temp)
         except Exception as e:
             print(e)
-            continue
-    
+
     return result
