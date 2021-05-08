45113d40864eb489a2f00b4c31a0a5608a14cb7d
onlinejudge95 <onlinejudge95@gmail.com>
2021-05-08 18:14:06 +0530

    Adds isort check to sort imports

--- bin/lint.sh
+++ bin/lint.sh
@@ -2,3 +2,4 @@
 
 black --check --diff --target-version py37 .
 flake8 --exclude __init__.py --max-line-length 88 .
+isort --atomic --check --force-single-line-imports --lines-after-imports 2 --lines-between-types 1 --line-length 88 --profile black .
