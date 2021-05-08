0af2498ed80c7a8f92b971e4f397c6dd488d9dc8
onlinejudge95 <onlinejudge95@gmail.com>
2021-05-08 15:54:17 +0530

    Implements initial scraping structure

--- src/blueprints/api/lib.py
+++ src/blueprints/api/lib.py
@@ -1,21 +1,21 @@
 def get_all_nodes(nodes):
     result = []
-    count = 0
 
     for node in nodes:
         try:
             data_nodes = node.find_all("td")
-            count += 1
-            country_name = data_nodes[1].find("a").contents[0]
             total_cases = int(data_nodes[2].contents[0].replace(",", ""))
-            print(total_cases)
+            total_recovered = int(data_nodes[6].contents[0].replace(",", ""))
+            population = int(data_nodes[14].contents[0].replace(",", ""))
             temp = {
-                "country_name": country_name,
+                "country_name": data_nodes[1].find("a").contents[0],
                 "total_cases": total_cases,
+                "active_cases": int(data_nodes[8].contents[0].replace(",", "")),
+                "total_deaths": int(data_nodes[4].contents[0].replace(",", "")),
+                "recovery_rate": round((total_recovered * 100.0) / total_cases, 2),
+                "infected_population_percentage": round((total_cases * 100.0) / population, 2)
             }
             result.append(temp)
-            if count == 10:
-                break
         except Exception as e:
             print(e)
             continue
