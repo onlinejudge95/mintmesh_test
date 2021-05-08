d373a4c307dfa1a89e8dfa9f49bb68b747af353e
onlinejudge95 <onlinejudge95@gmail.com>
2021-05-08 14:13:33 +0530

    Scrapes the given website till tbody node

--- config/settings.py
+++ config/settings.py
@@ -2,3 +2,4 @@ from os import getenv
 
 
 DEBUG = getenv("DEBUG_MODE", False)
+DATA_SOURCE_URL = "https://www.worldometers.info/coronavirus/"
--- src/blueprints/api/views.py
+++ src/blueprints/api/views.py
@@ -1,5 +1,9 @@
+from bs4 import BeautifulSoup
 from flask import Blueprint
+from flask import current_app
 from flask import jsonify
+from lxml import etree
+from requests import get
 
 
 bp = Blueprint("api", __name__, url_prefix="/api")
@@ -7,7 +11,17 @@ bp = Blueprint("api", __name__, url_prefix="/api")
 
 @bp.route("/case_details")
 def global_case_details():
-    pass
+    url = current_app.config.get("DATA_SOURCE_URL")
+    try:
+        response = get(url)
+
+        if response.status_code == 200:
+            soup = BeautifulSoup(response.content, "lxml")
+
+            node = soup.find("table", {"id": "main_table_countries_today"}).find("tbody")
+        return jsonify({"url": url, "data": []}), 200
+    except Exception as e:
+        return jsonify({"status": "error", "message": str(e)}), 500
 
 
 @bp.route("/case_details/<string:country>")
